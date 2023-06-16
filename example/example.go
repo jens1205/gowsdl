17a0b8fc08a2a186133853e018f96a8ab41b15ea
Jens Gersdorf <jens.gersdorf@telekom.de>
2023-06-16 10:05:37 +0200

    module changed to jens1205 fork

--- example/example.go
+++ example/example.go
@@ -5,8 +5,8 @@ import (
 	"log"
 	"time"
 
-	"github.com/hooklift/gowsdl/example/gen"
-	"github.com/hooklift/gowsdl/soap"
+	"github.com/jens1205/gowsdl/example/gen"
+	"github.com/jens1205/gowsdl/soap"
 )
 
 func ExampleBasicUsage() {
--- header_tmpl.go
+++ header_tmpl.go
@@ -13,7 +13,7 @@ import (
 	"context"
 	"encoding/xml"
 	"time"
-	"github.com/hooklift/gowsdl/soap"
+	"github.com/jens1205/gowsdl/soap"
 
 	{{/*range .Imports*/}}
 		{{/*.*/}}
